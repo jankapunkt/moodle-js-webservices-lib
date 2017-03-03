9c779c113994252ab7885fa92634ac33d00d6cc4
jankapunkt <jkuester@uni-bremen.de>
2017-03-03 16:59:54 +0100

    updated formatting

--- build.js
+++ build.js
@@ -1,21 +1,12 @@
-/*
-	
-	Summary: 
-
-	NodeJS build script to support babel build on windows and OSX/Linux in one script
-	Run this file in your npm script (e.g. prepublish) via "node babel_build.js"
-*/
-
-
 const os = require('os');
 const exec = require('child_process').exec;
 
-console.log("-----------------------------");
+console.log("---------------------------------------------");
 console.log("BABEL BUILDING FOR PLATFORM " + os.platform());
-console.log("-----------------------------");
+console.log("---------------------------------------------");
 
-if (os.platform() === "win32"){
-	exec('if exist %cd%\dist rmdir %cd%\dist else echo "dist does not exist, continue with build" ', function (err, stdout, stderr) {
+if (os.platform() === "win32") {
+	exec('if exist %cd%\dist rmdir %cd%\dist', function (err, stdout, stderr) {
 		if (err)
 			throw err;
 		if (stderr)
@@ -32,7 +23,7 @@ if (os.platform() === "win32"){
 			});
 		}
 	});
-}else{
+} else {
 	exec('rm -rf dist/** && babel lib --out-dir dist --ignore *.tests.js,testHelpers/*', function (err, stdout, stderr) {
 		if (err) 
 			throw err;
